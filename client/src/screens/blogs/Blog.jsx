d5cde16ff59b93826ab373b764c1a3773f86618b
bishnudev1 <bishnudevkhutia@gmail.com>
2023-04-26 21:37:38 +0530

    blogs-ui-updated

--- client/src/screens/blogs/Blog.jsx
+++ client/src/screens/blogs/Blog.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Heading, VStack, Text, HStack, Image } from '@chakra-ui/react';
+import { Box, Heading, VStack, Text, HStack, Image, Container } from '@chakra-ui/react';
 import { SiFacebook, SiLinkedin, SiStackoverflow } from 'react-icons/si';
 import { useLocation } from 'react-router-dom';
 
@@ -10,17 +10,17 @@ const Blog = () => {
     const blogData = location.state ? location.state : null;
 
     return (
-        <Box bgColor={'white'} minH={"100vh"} p={["4", "16"]}>
+        <Container minH={"100vh"} maxW={"container.xl"} p={["4", "16"]}>
             {
                 blogData === null ? <Heading textAlign={"center"} size={"lg"}>Loading...</Heading>
                     :
                     <VStack spacing={"10"} px={["4", "8"]} alignItems={"flex-start"}>
-                        <Heading textAlign={"left"} color={'green'} size={"3xl"}>{blogData.title}</Heading>
-                        <Image objectFit={'contain'} boxSize={["unset", "80"]} src={blogData.image.url} />
-                        <Text fontFamily={"inherit"} fontWeight={"bold"} color={'green.800'} fontSize={"2xl"}>{blogData.desc}</Text>
+                        <Image objectFit={'contain'} src={blogData.image.url} />
+                        <Heading textAlign={"left"} color={'white'} size={"2xl"}>{blogData.title}</Heading>
+                        <Text fontFamily={"inherit"} fontWeight={"bold"} color={'white'} fontSize={["xl","2xl"]}>{blogData.desc}</Text>
                         <HStack pt={["4", "6"]} spacing={["4", "8"]} justifyContent={"space-evenly"} alignItems={["center", "flex-end"]}>
-                            <Text fontWeight={"bold"} color={'green.600'} fontFamily={"inherit"}>{`Published on ${blogData.createdAt}`}</Text>
-                            <Text fontWeight={"bold"} color={'green.600'} fontFamily={"inherit"}>{`Written by ${blogData.createdBy}`}</Text>
+                            <Text fontWeight={"bold"} color={'white'} fontFamily={"inherit"}>{`Published on ${blogData.createdAt}`}</Text>
+                            <Text fontWeight={"bold"} color={'white'} fontFamily={"inherit"}>{`Written by ${blogData.createdBy}`}</Text>
                         </HStack>
                         <HStack width={["100%", "unset"]} spacing={["4", "8"]} justifyContent={"space-evenly"}>
                             <a href='www.google.co.in'><SiFacebook size={'40'} color='skyblue' /></a>
@@ -29,7 +29,7 @@ const Blog = () => {
                         </HStack>
                     </VStack>
             }
-        </Box>
+        </Container>
     )
 }
 
